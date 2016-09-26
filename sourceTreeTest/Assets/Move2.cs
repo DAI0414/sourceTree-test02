dede662ea2b498bfc7f75a2e455cbd8f8602f304
DAI0414 <terao@systemfriend.co.jp>
2016-09-26 17:12:50 +0900

    Resourcesフォルダ追加 Prefabフォルダ追加 スクリプトの追加

---
+++ sourceTreeTest/Assets/Move2.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move2 : MonoBehaviour {
+
+    [Range(1, 10)]
+    public float angle = 0f;
+    [Range(0, 10)]
+    public float range = 4f;
+    [Range(0.01f, 1f)]
+    public float yspeed = 0.05f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = this.gameObject.transform.localPosition;
+        this.gameObject.transform.localPosition = new Vector3(pos.x, Mathf.Cos(angle) * range, pos.z);
+        angle += yspeed;
+    }
+}
